1fd304b6cf5f8a13d0d38a1470784c6269998420
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-03-17 21:48:44 +0530

    cofiguration added breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["{./src/**/*.{html,js}"],
   theme: {
+    screens:{
+      sm: '486px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
     extend: {},
   },
   plugins: [],
